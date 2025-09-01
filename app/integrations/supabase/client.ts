a96da9fca7f145f81ffcb10da09970bf42995a2d
Sully-App <sullymanagementapp@gmail.com>
2025-09-01 13:10:02 -0400

    Update client.ts

--- app/integrations/supabase/client.ts
+++ app/integrations/supabase/client.ts
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import type { Database } from './types';
 import { createClient } from '@supabase/supabase-js'
 
-export const SUPABASE_URL = "https://igacfofofodntrsmehdf.supabase.co";
+export const SUPABASE_URL = "https://btxjjzcdirbkjwerveox.supabase.co";
 export const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0eGpqemNkaXJia2p3ZXJ2ZW94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2NzMwODQsImV4cCI6MjA3MjI0OTA4NH0.dbMqrPA2aYn0jyCsR69OaoA16EFHjMc2gzTSKeIo70g";
 
 // Import the supabase client like this:
