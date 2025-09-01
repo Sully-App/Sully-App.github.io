87877845e4bfba5d678715af1bd374cd379c9bc7
Sully-App <sullymanagementapp@gmail.com>
2025-09-01 16:45:15 -0400

    Update index.tsx

--- app/(app)/profile/index.tsx
+++ app/(app)/profile/index.tsx
@@ -5,7 +5,7 @@ import * as ImagePicker from 'expo-image-picker';
 import { colors, commonStyles } from '../../../styles/commonStyles';
 import Button from '../../../components/Button';
 import { useAppState } from '../../../store/AppStateContext';
-import SettingSheet from '../../../components/SettingSheet';
+import SettingsSheet from '../../../components/SettingsSheet';
 
 export default function ProfileScreen() {
   const { role, accounts, currentAccount, updateAccount, setSchedule } = useAppState();
