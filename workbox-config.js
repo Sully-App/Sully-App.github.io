5ac8cb4643e2658a74f206979d79e15dbe702734
Sully-App <sullymanagementapp@gmail.com>
2025-09-05 14:43:34 -0400

    Update workbox-config.js

--- workbox-config.js
+++ workbox-config.js
@@ -1,23 +1,19 @@
 module.exports = {
-	globDirectory: 'dist',
-	globPatterns: [
-		'**/*.{js,css,html,png,jpg,jpeg,svg,ico,json}'
-	],
-	swDest: 'dist/sw.js',
-	skipWaiting: true,
-	clientsClaim: true,
-	navigationPreload: true,
-	runtimeCaching: [{
-		urlPattern: /.*/,
-		handler: 'NetworkFirst',
-		options: {
-			networkTimeoutSeconds: 10,
-			backgroundSync: {
-				name: 'navigation-queue',
-				options: {
-					maxRetentionTime: 24 * 60 // 24 hours
-				}
-			}
-		}
-	}]
+  globDirectory: '_expo/web',
+  globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,svg,ico,json}'],
+  swDest: '_expo/web/sw.js',
+  skipWaiting: true,
+  clientsClaim: true,
+  navigationPreload: true,
+  runtimeCaching: [{
+    urlPattern: /.*/,
+    handler: 'NetworkFirst',
+    options: {
+      networkTimeoutSeconds: 10,
+      backgroundSync: {
+        name: 'navigation-queue',
+        options: { maxRetentionTime: 24 * 60 } // 24 hours
+      }
+    }
+  }]
 };
